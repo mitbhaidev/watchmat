e68b5df134b1faae0dde3225c6ae0d6c7cf3822f
mitbhaidev <mitbhai989@gmail.com>
2025-08-06 16:24:02 +0530

    media remove

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+
 from environ import Env
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -173,8 +174,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
