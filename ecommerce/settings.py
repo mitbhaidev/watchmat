e15e5f8fddc4e0797abd054ae2fea370dbdaa38f
mitbhaidev <mitbhai989@gmail.com>
2025-08-06 17:38:08 +0530

    problem worked

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,13 +42,11 @@ INSTALLED_APPS = [
 ]
 
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': env('CLOUDINARY_API_KEY'),
-    'API_SECRET': env('CLOUDINARY_API_SECRET'),
-}
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE ={
+    'CLOUDINARY_URL'== env ('CLOUDINARY_URL')
+}
 
 
 
