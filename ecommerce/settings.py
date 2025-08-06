418cbdc543510d46f65c339745603d069c3d6367
mitbhaidev <mitbhai989@gmail.com>
2025-08-06 19:29:02 +0530

    problem solve

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -175,6 +175,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+
+# Don't use this if you're not storing images locally
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
